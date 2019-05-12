1e3558ebe09d0d068d57b4cd85f30037a040cafd
Haofan Zheng <hazheng@mix.wvu.edu>
2019-05-11 20:30:08 -0700

    Added DecentDhtNonEnclaveBinding Java class.

---
+++ sources/DhtClientYcsb_Java/com/decent/dht/DhtClientNonEnclaveBinding.java
@@ -0,0 +1,27 @@
+package com.decent.dht;
+
+public class DhtClientNonEnclaveBinding
+{
+	static
+	{
+		System.loadLibrary("DhtClientNonEnclaveYcsb_App"); // Load native library.
+	}
+
+	public DhtClientNonEnclaveBinding()
+	{
+		this.cPtr = 0;
+	}
+
+	public native void init() throws DhtClientBindingException;
+
+	public native void cleanup() throws DhtClientBindingException;
+
+	public native String read(String key) throws DhtClientBindingException;
+
+	public native void insert(String key, String value) throws DhtClientBindingException;
+
+	public native void delete(String key) throws DhtClientBindingException;
+
+	private long cPtr;
+
+}
