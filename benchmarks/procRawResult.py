0ef79854eb3a0682c884b7fffef047b880082400
Haofan Zheng <hazheng@mix.wvu.edu>
2019-08-09 17:45:59 -0700

    impl auto process through all csv under a dir. (for csv data analytic script)

--- benchmarks/procRawResult.py
+++ benchmarks/procRawResult.py
@@ -1,7 +1,10 @@
 import os
 import sys
 import argparse
+import progressbar
 import pandas as pd
+
+COLUMN_NAMES = ['Name_Prefix', 'Attempt', 'Num_of_Node', 'Num_of_Thread', 'Ops_per_Session', 'Ops', 'Time_Elapsed_ms', 'Throughput_op_per_s', 'Percentile_Latency_us']
 
 def PreProcCsv(oriFilePath, resFilePath):
 
@@ -54,24 +57,77 @@ def ProcOneResultSet(inputfilepath, warmUpTime, percentile):
 	throughtput = totalOps / timeElapsedS
 	latencyPer = dataF.Latency_us.quantile(q=(percentile / 100.0), interpolation='higher')
 
-	return totalOps, timeElapsedMs, throughtput, latencyPer
+	return [totalOps, timeElapsedMs, throughtput, latencyPer]
+
+def ParseFileName(filepath):
+
+	root, ext = os.path.splitext(filepath)
+	filename = os.path.basename(root)
+
+	components = filename.split('_')
+
+	return components
+
+def SummaryOneResultSet(filepath, warmUpTime, percentile):
+
+	filenameRes = ParseFileName(filepath)
+	dataRes = ProcOneResultSet(filepath, warmUpTime, percentile)
+
+	return filenameRes + dataRes
 
 def main():
 
+	print()
+
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--file', required=True)
+	parser.add_argument('--dir', required=True)
 	parser.add_argument('--warmUpTime', required=True, type=int)
 	parser.add_argument('--percentile', required=True, type=int)
+	parser.add_argument('--out', required=True)
 
 	args = parser.parse_args()
 
-	totalOps, timeElapsedMs, throughtput, latencyPer = ProcOneResultSet(args.file, args.warmUpTime, args.percentile)
+	#Convert directory path to absolute path
+	dirPath = os.path.abspath(args.dir)
 
-	print('')
-	print('Total OPs:', str(totalOps))
-	print('Throughput(ops/s):', str(throughtput))
-	print(str(args.percentile), 'Percentil_latency(us):', str(latencyPer))
-	print('')
+	if (not os.path.exists(dirPath)) or (os.path.isfile(dirPath)):
+		print('FATAL_ERR:', 'The input directory:', dirPath, 'does not exist, or it is a file!')
+		exit(-1)
+
+	outPath = os.path.abspath(args.out)
+
+	if os.path.exists(outPath):
+		print('FATAL_ERR:', 'The output file path:', outPath, 'already exist!')
+		exit(-1)
+
+	rows = []
+
+	print('INFO:', 'Processing...')
+
+	proBar = progressbar.ProgressBar()
+
+	#iterate through files in the directory
+	for filename in proBar(os.listdir(dirPath)):
+
+		#looking for all csv files
+		if filename.endswith(".csv"):
+
+			filepath = os.path.join(dirPath, filename)
+
+			#Make sure it is a file (not a directory)
+			if os.path.isfile(filepath):
+
+				#print('.', end='', flush=True)
+				row = SummaryOneResultSet(filepath, args.warmUpTime, args.percentile)
+				rows.append(row)
+
+	summaryDataF = pd.DataFrame(data=rows, columns=COLUMN_NAMES)
+
+	print('INFO:', 'Writting results into', outPath, '...')
+
+	summaryDataF.to_csv(outPath)
+
+	print()
 
 if __name__ == '__main__':
 	sys.exit(main())
