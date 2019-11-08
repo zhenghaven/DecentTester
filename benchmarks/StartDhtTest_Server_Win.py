f39397590b4f304f7552c45d5a521d308c997a1e
Haofan Zheng <hazheng@mix.wvu.edu>
2019-11-07 20:08:04 -0800

    Made tester - svr side ready for config file setup.

--- benchmarks/StartDhtTest_Server_Win.py
+++ benchmarks/StartDhtTest_Server_Win.py
@@ -8,37 +8,19 @@ import subprocess
 import SocketTools as st
 import procConfigureTools as pct
 
-AFINITY_LIST = [[3], [4], [5], [7], [6], [1]]
-DEFAULT_PRIORITY = psutil.REALTIME_PRIORITY_CLASS
+def ConfigSysProc(svcBinNameList, svcAffList, svcPriority):
 
-DHT_PORT_START = 57756
+	for svcBinName in svcBinNameList:
+		for p in pct.FindProcsByName(svcBinName):
+			p.nice(svcPriority)
+			p.cpu_affinity(svcAffList)
 
-DHT_PROG_NAME = 'DecentDht_App'
-
-SYS_REL_AESM_NAME = 'aesm_service'
-SYS_REL_WIN_NET_NAME = 'WmiPrvSE'
-SYS_REL_AFINITY_LIST = [0, 2]
-SYS_REL_DEFAULT_PRIORITY = psutil.HIGH_PRIORITY_CLASS
-
-TEST_SERVER_ADDR = ''
-TEST_SERVER_PORT = 57725
-
-def ConfigSysProc():
-
-	for p in pct.FindProcsByName(SYS_REL_AESM_NAME):
-		p.nice(SYS_REL_DEFAULT_PRIORITY)
-		p.cpu_affinity(SYS_REL_AFINITY_LIST)
-
-	for p in pct.FindProcsByName(SYS_REL_WIN_NET_NAME):
-		p.nice(SYS_REL_DEFAULT_PRIORITY)
-		p.cpu_affinity(SYS_REL_AFINITY_LIST)
-
-def SetupTestServer():
+def SetupTestServer(testerSvrAddr, testerSvrPort):
 
 	print('INFO:', 'Setting up the server...')
 
 	svr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	svr.bind((TEST_SERVER_ADDR, TEST_SERVER_PORT))
+	svr.bind((testerSvrAddr, testerSvrPort))
 	svr.listen(1)
 
 	return svr
@@ -86,11 +68,11 @@ def AcceptTestClients(svr):
 
 	return connArr
 
-def RecvNumOfNode(conn):
+def RecvNumOfNode(conn, maxNumOfNode):
 
 	numOfNode = st.SocketRecv_uint64(conn)
 
-	if numOfNode > len(AFINITY_LIST) or numOfNode < 1:
+	if not (1 <= numOfNode and numOfNode <= maxNumOfNode):
 		st.SocketSendPack(conn, 'Err')
 		raise RuntimeError('The total number of node given (i.e. ' + numOfNode + ') is out of range!')
 	else:
@@ -111,12 +93,12 @@ def WaitFor(sec):
 
 	print()
 
-def RunTestProgram(numOfNode):
+def RunTestProgram(numOfNode, targetBin, targetPortStart):
 
 	cWorkDir = os.getcwd()
-	dhtAppPath = os.path.join(cWorkDir, DHT_PROG_NAME)
+	dhtAppPath = os.path.join(cWorkDir, targetBin)
 
-	currPortI = DHT_PORT_START + numOfNode - 1
+	currPortI = targetPortStart + numOfNode - 1
 	prevPortI = currPortI
 
 	procObjs = []
@@ -161,7 +143,7 @@ def KillTestProgram(procObjs):
 	for procObj in procObjs:
 		procObj.kill()
 
-def GetClientsStartEndRound(connArr, pList, numOfNode):
+def GetClientsStartEndRound(connArr, pList, targetAffList, svcAffList):
 
 	for conn in connArr:
 		clientSignal = st.SocketRecvPack(conn)
@@ -169,7 +151,7 @@ def GetClientsStartEndRound(connArr, pList, numOfNode):
 		if clientSignal != 'Start':
 			return False
 
-	sysStatRecorder = pct.SysStatusRecorderThread(pList, sum(AFINITY_LIST[:numOfNode], []), SYS_REL_AFINITY_LIST, 0.1) # Start
+	sysStatRecorder = pct.SysStatusRecorderThread(pList, sum(targetAffList, []), svcAffList, 0.1) # Start
 	sysStatRecorder.start()
 
 	for conn in connArr:
@@ -196,55 +178,72 @@ def GetClientsStartEndRound(connArr, pList, numOfNode):
 
 	return True
 
-def RunOneTestCase(connArr):
-
-	ConfigSysProc()
+def RunOneTestCase(connArr, targetBin, targetAffFullList, targetPriority, targetPortStart, svcBinNameList, svcAffFullList, svcPriority):
 
 	#Recv num of server nodes to spawn
-	numOfNode = RecvNumOfNode(connArr[0])
+	numOfNode = RecvNumOfNode(connArr[0], len(targetAffFullList))
+
+	svcAffList = svcAffFullList[:numOfNode]
+	targetAffList = targetAffFullList[:numOfNode]
+	ConfigSysProc(svcBinNameList, svcAffList, svcPriority)
 
 	procObjs = None
 
 	try:
 		#Run server nodes:
 		print('INFO:', 'Spawning', numOfNode, 'nodes...')
-		procObjs = RunTestProgram(numOfNode)
+		procObjs = RunTestProgram(numOfNode, targetBin, targetPortStart)
 
 		pidList = GetPidListFromProcObjs(procObjs)
 
 		#Configure CPU affinity & priority etc...
-		pList = pct.ConfigureProc(pidList, AFINITY_LIST, DEFAULT_PRIORITY)
+		pList = pct.ConfigureProc(pidList, targetAffList, targetPriority)
 
 		for conn in connArr:
 			#Now, server is ready
 			st.SocketSendPack(conn, 'R')
 			print('INFO:', 'Server is ready.')
 
-		isClientFinished = GetClientsStartEndRound(connArr, pList, numOfNode)
+		isClientFinished = GetClientsStartEndRound(connArr, pList, targetAffList, svcAffList)
 
 		while isClientFinished:
-			isClientFinished = GetClientsStartEndRound(connArr, pList, numOfNode)
+			isClientFinished = GetClientsStartEndRound(connArr, pList, targetAffList, svcAffList)
 
 		print('INFO:', 'Client has finished the current test case.')
 
 	finally:
 		KillTestProgram(procObjs)
 
-def ListenToClients(connArr):
+def ListenToClients(connArr, targetBin, targetAffFullList, targetPriority, targetPortStart, svcBinNameList, svcAffFullList, svcPriority):
 
 	clientSignal = st.SocketRecvPack(connArr[0])
 
 	while clientSignal == 'Test':
-		RunOneTestCase(connArr)
+		RunOneTestCase(connArr, targetBin, targetAffFullList, targetPriority, targetPortStart, svcBinNameList, svcAffFullList, svcPriority)
 		clientSignal = st.SocketRecvPack(connArr[0])
+
+TEST_TARGET_BINARY = 'DecentDht_App'
+TEST_TARGET_AFINITY_LIST = [[1], [3], [5], [7], [9], [11], [13], [15]]
+TEST_TARGET_PRIORITY = psutil.REALTIME_PRIORITY_CLASS
+TEST_TARGET_PORT_START = 57756
+
+SYS_SVC_AESM_NAME = 'aesm_service'
+SYS_SVC_WIN_NET_NAME = 'WmiPrvSE'
+SYS_SVC_PYTHON_NAME = 'python'
+SYS_SVC_AFINITY_LIST = [0, 2, 4, 6, 8, 10, 12, 14]
+SYS_SVC_DEFAULT_PRIORITY = psutil.HIGH_PRIORITY_CLASS
+
+TESTER_SERVER_ADDR = ''
+TESTER_SERVER_PORT = 57725
 
 def main():
 
-	svr = SetupTestServer()
+	svr = SetupTestServer(TESTER_SERVER_ADDR, TESTER_SERVER_PORT)
 
 	connArr = AcceptTestClients(svr)
 
-	ListenToClients(connArr)
+	ListenToClients(connArr, TEST_TARGET_BINARY, TEST_TARGET_AFINITY_LIST, TEST_TARGET_PRIORITY, TEST_TARGET_PORT_START
+		            [SYS_SVC_AESM_NAME, SYS_SVC_WIN_NET_NAME, SYS_SVC_PYTHON_NAME], SYS_SVC_AFINITY_LIST, SYS_SVC_DEFAULT_PRIORITY)
 
 	print('INFO:', 'Tests are finished!')
 
